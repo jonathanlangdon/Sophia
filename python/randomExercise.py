5ec5034a516872c3a5b852ae429a0de8adfb9b99
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-12-31 19:34:50 -0500

    split into groups of 3 function added

--- python/randomExercise.py
+++ python/randomExercise.py
@@ -1,8 +1,19 @@
+import random
+
+
 def randomExercises(filename):
     try:
         with open(filename, "r") as file:
             exercises = file.readlines()
             random.shuffle(exercises)
+            print(split_into_groups_of_three(exercises))
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+
+
+def split_into_groups_of_three(list_exercises):
+    list_of_groups = []
+    for i in range(0, len(list_exercises), 3):
+        list_of_groups.append(list_exercises[i : i + 3])
+    return list_of_groups
